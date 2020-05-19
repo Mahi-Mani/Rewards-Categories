db4d2d23c3372201ad769aa81b9764145094ce76
Mahisha Gunasekaran <m.mahishamani@gmail.com>
2020-05-19 14:17:19 -0700

    Code cleanup

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,13 @@
-// $(document).ready(function (){
-// function allowDrop(ev) {
-//     console.log("1");
-//     ev.preventDefault();
-// }
-
-// function drag(ev) {
-//     console.log(ev.target.id);
-//     ev.dataTransfer.setData("text", ev.target.id);
-// }
-
-// function drop(ev) {
-//     ev.preventDefault();
-//     console.log(event.dataTransfer);
-//     var data = ev.dataTransfer.getData("text");
-//     console.log(data);
-//     ev.target.appendChild(document.getElementById(data));
-// }
-
-document.addEventListener("dragstart", function(event) {
+document.addEventListener("dragstart", function (event) {
     event.dataTransfer.setData("Text", event.target.id);
-  });
-  
-  /* Events fired on the drop target */
-  document.addEventListener("dragover", function(event) {
+});
+
+/* Events fired on the drop target */
+document.addEventListener("dragover", function (event) {
     event.preventDefault();
-  });
-  
-  document.addEventListener("drop", function(event) {
+});
+
+document.addEventListener("drop", function (event) {
     event.preventDefault();
     var data = event.dataTransfer.getData("Text");
     console.log(data);
@@ -36,24 +17,25 @@ document.addEventListener("dragstart", function(event) {
     var upperCaseData = value.toUpperCase();
     event.target.appendChild(document.getElementById(data));
     $("." + value).html(`<div class="card droptarget" id=${data}-${number} draggable="true" style="width: 5rem; height: 5rem;">
+    <span class="close-icon float-right"><i class="fa fa-times"></i></span>
     <div class="card-body">
         <span>${upperCaseData}</span>
     </div>
 </div>`);
-    $(`#${data}`).on("click", function(event) {
+    $(`#${data}`).on("click", function (event) {
         event.preventDefault();
         console.log("I'm clicked", this.id);
         $(`#${data}`).remove();
     })
-  });
+});
 
 function generateHeading() {
     var rowHeading = $("<div>");
     rowHeading.addClass("row heading");
     // console.log(rowHeading);
-    for (var i = 0; i <= 5; i++) {
+    for (var i = 0; i <= 6; i++) {
         var cols = $("<div>");
-        cols.addClass("col-md-2");
+        cols.addClass("col-md-1");
         if (i === 0) {
             cols.text("Heading");
         } else {
@@ -68,33 +50,18 @@ function generateHeading() {
 
 function generateNextRows() {
     var rowNum = 0;
-    while (rowNum < 5) {
+    while (rowNum < 6) {
         var newRow = $("<div>");
         newRow.addClass("row reward" + i);
-        for (var i = 0; i < 5; i++) {
+        for (var i = 0; i < 6; i++) {
             var newCol = $("<div>");
-            newCol.addClass("col-md-2 droptarget");
+            newCol.addClass("col-md-1 droptarget");
             newCol.attr("id", "c" + i);
             if (i === 0) {
                 newCol.text("R" + (rowNum + 1));
                 newCol.removeClass("droptarget");
             }
-            // newCol.attr("ondrop", drop(event));
-            // newCol.attr("ondragover", allowDrop(event));
             newRow.append(newCol);
-            // $("#c" + i).on("drop", function (event) {
-            //     event.preventDefault();
-            //     console.log(event.target.id);
-            // });
-            // $("#c" + i).on("dragover", function (event) {
-            //     console.log("1");
-            //     event.preventDefault();
-            //     // drag(event);
-            // });
-            // function drag(ev) {
-            //     console.log(ev.target.id);
-            //     ev.dataTransfer.setData("text", ev.target.id);
-            // }
         }
         $("#nextRows").append(newRow);
         $("#nextRows").append("<br>");
@@ -102,6 +69,21 @@ function generateNextRows() {
     }
 }
 
+function generateRewards() {
+    for (var i = 0; i < 6; i++) {
+        var mainDiv = $("<div>");
+        mainDiv.addClass("r" + (i+1));
+        mainDiv.append(`<div class="card droptarget" id=r${i+1}-0 draggable="true" style="width: 5rem; height: 5rem;">
+        <span class="close-icon float-right"><i class="fa fa-times"></i></span>
+        <div class="card-body">
+            <span><small>R${i + 1}</small></span>
+        </div>
+    </div>`);
+    $("#rewards").append(mainDiv);
+    }
+    
+}
+
 generateHeading();
 generateNextRows();
-// })
+generateRewards();
